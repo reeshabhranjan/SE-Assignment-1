f76aaedd0d5622a63767bf4cd95582d9624198ad
Reeshabh Kumar Ranjan <reeshabhkumarranjan@gmail.com>
2020-01-08 22:00:38 +0530

    added code to get basic info of the file

--- mysudo.c
+++ mysudo.c
@@ -2,6 +2,8 @@
 #include<unistd.h>
 #include<sys/stat.h>
 #include<sys/types.h>
+#include<pwd.h>
+#include<grp.h>
 
 int main(int argc, char** argv)
 {
@@ -12,15 +14,18 @@ int main(int argc, char** argv)
 	}
 	char *filename = argv[1]; // assuming argv[1] contains path
 	FILE *fp = fopen(filename, "w+");
-	int ruid = getuid();
-	int euid = geteuid();
+	int ruid_caller = getuid();
+	int euid_caller = geteuid();
 	fclose(fp);
 
 	// getting the stat struct for the file
 	struct stat st;
-
 	stat(filename, &st);
-	printf("%d\n", st.st_mode & S_IXOTH);
+	int exec_user = st.st_mode & S_IXUSR;
+	int exec_group = st.st_mode & S_IXGRP;
+	int exec_other = st.st_mode & S_IXOTH;
+	int uid_owner = getpwuid(st.st_uid);
+	int gid_owner = getgrgid(st.st_gid);
 
 	return 0;
 }
