46d4393fb6dc27dd8b6692218d7424a736a31350
Reeshabh Kumar Ranjan <reeshabhkumarranjan@gmail.com>
2020-01-08 22:57:50 +0530

    added code to extract uid from file

--- mysudo.c
+++ mysudo.c
@@ -12,7 +12,17 @@ int main(int argc, char** argv)
 		printf("Enter an argument.\n");
 		return 0;
 	}
+
 	char *filename = argv[1]; // assuming argv[1] contains path
+
+	int file_exists = access(filename, F_OK) + 1; // 1 for exists and 0 otherwise
+
+	if (!file_exists)
+	{
+		printf("Cannot find command.\n");
+		return 0;
+	}
+
 	FILE *fp = fopen(filename, "w+");
 	int ruid_caller = getuid();
 	int euid_caller = geteuid();
@@ -24,8 +34,11 @@ int main(int argc, char** argv)
 	int exec_user = st.st_mode & S_IXUSR;
 	int exec_group = st.st_mode & S_IXGRP;
 	int exec_other = st.st_mode & S_IXOTH;
-	int uid_owner = getpwuid(st.st_uid);
-	int gid_owner = getgrgid(st.st_gid);
+	int uid_owner = st.st_uid;
+	int gid_owner = st.st_gid;
+
+	printf("uid_caller: %d\n", ruid_caller);
+	printf("uid_owner:  %d\n", uid_owner);
 
 	return 0;
 }
