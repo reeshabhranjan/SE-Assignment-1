6dfc4ce3174b07a934d4fc90e444d11270703e06
Reeshabh Kumar Ranjan <reeshabhkumarranjan@gmail.com>
2020-01-10 02:18:11 +0530

    added code for handling improper permissions

--- mysudo.c
+++ mysudo.c
@@ -7,38 +7,58 @@
 
 int main(int argc, char** argv)
 {
+	// checking argument count
 	if (argc <= 1) // because argc can be 0 in some cases
 	{
 		printf("Enter an argument.\n");
 		return 0;
 	}
 
+	// checking existence of file
 	char *filename = argv[1]; // assuming argv[1] contains path
-
 	int file_exists = access(filename, F_OK) + 1; // 1 for exists and 0 otherwise
-
 	if (!file_exists)
 	{
 		printf("Cannot find command.\n");
 		return 0;
 	}
 
-	FILE *fp = fopen(filename, "w+");
+	// getting information about the caller
 	int ruid_caller = getuid();
 	int euid_caller = geteuid();
-	fclose(fp);
 
-	// getting the stat struct for the file
+	printf("%d\n", euid_caller);
+
+	// getting the stat struct and info for the file
 	struct stat st;
 	stat(filename, &st);
 	int exec_user = st.st_mode & S_IXUSR;
 	int exec_group = st.st_mode & S_IXGRP;
 	int exec_other = st.st_mode & S_IXOTH;
+	int setuid_bit = st.st_mode & S_ISUID;
+	int setguid_bit = st.st_mode & S_ISGID;
 	int uid_owner = st.st_uid;
 	int gid_owner = st.st_gid;
 
-	printf("uid_caller: %d\n", ruid_caller);
-	printf("uid_owner:  %d\n", uid_owner);
+	// checking file permissions
+	// checking if owner has the execute permission
+	if (!exec_user)
+	{
+		printf("Permission denied. (owner doesn't have execute permission)\n");
+		return 0;
+	}
+
+	if (!setuid_bit)
+	{
+		printf("Permission denied. (file doesn't have a setuid_bit bit set)\n");
+		return 0;
+	}
+	else
+	{
+		// TODO ask for password
+		
+
+	}
 
 	return 0;
 }
