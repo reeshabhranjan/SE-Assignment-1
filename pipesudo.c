35fa93e5f2268c8a73b1cb54164a672b62f50e51
Reeshabh Kumar Ranjan <reeshabhkumarranjan@gmail.com>
2020-01-22 21:34:04 +0530

    added some debug print statements

--- pipesudo.c
+++ pipesudo.c
@@ -20,24 +20,39 @@ int main(int argc, char **argv)
 
     // OK
 
-    char* command1 = argv[0];
-    char* command2 = argv[pipe_position + 1];
     char** command1_args = (char **)(malloc(sizeof(char*) * (pipe_position + 1)));
+    char** command2_args = (char **)(malloc(sizeof(char*) * (argc - pipe_position)));
     for (int i = 0; i < pipe_position; i++)
     {
-        // command1_args[i] = (char *)(malloc(sizeof(char) * (strlen(argv[i]) + 1)));
-        // strcpy(command1_args[i], argv[i]);
         command1_args[i] = strdup(argv[i]);
     }
 
+    for (int i = pipe_position + 1; i < argc; i++)
+    {
+        command2_args[i] = strdup(argv[i]);
+    }
+    
     command1_args[pipe_position] = NULL;
+    command2_args[argc - pipe_position - 1] = NULL;
 
-    int fd[2]; // 0 is the reading end and 1 is the writing end
-    // in fd table, 0 is stdin, 1 is stdout, 2 is stderr
+    for (int i = 0; i < pipe_position + 1; i++)
+    {
+        printf("%s ", command1_args[i]);
+    }
+    printf("\n");
+
+    for (int i = 0; i < argc - pipe_position; i++)
+    {
+        printf("%s ", command1_args[i]);
+    }
+    printf("\n");
+    
+
+    int fd[2];
 
     if (pipe(fd) == -1)
     {
-        perror("internal failure");
+        perror("Internal failure");
     }
 
     if (fork() == 0)
@@ -47,7 +62,10 @@ int main(int argc, char **argv)
         close(1);
         dup(fd[1]);
         close(fd[0]);
+
+        // char* arguments[] = {"/bin/ls"};
         execvp(command1_args[0], command1_args);
+        // execvp(arguments[0], arguments);
     }
     else
     {
@@ -56,7 +74,9 @@ int main(int argc, char **argv)
         close(0);
         dup(fd[0]);
         close(fd[1]);
-        execvp(argv[pipe_position + 1], argv + pipe_position + 1);
+        // char* arguments[] = {"/usr/bin/wc", "-l"};
+        // execvp(arguments[0], arguments);
+        execvp(command2_args[0], command2_args);
     }
     
     
